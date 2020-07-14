7111fbb8db0f2f6de5fe9c0960edc73b5224b7fd
Hyung-Taik Choi <htc.refactor@gmail.com>
2020-07-14 21:55:53 +0900

    Added example response body to Match class.

--- src/main/java/org/ajou/realcoding/leaguestats/domain/Match.java
+++ src/main/java/org/ajou/realcoding/leaguestats/domain/Match.java
@@ -1,4 +1,48 @@
 package org.ajou.realcoding.leaguestats.domain;
 
 public class Match {
+    /*
+    {
+        "matches": [
+            {
+                "platformId": "KR",
+                "gameId": 4492990456,
+                "champion": 517,
+                "queue": 450,
+                "season": 13,
+                "timestamp": 1594055812327,
+                "role": "DUO_SUPPORT",
+                "lane": "MID"
+            },
+            {
+                "platformId": "KR",
+                "gameId": 4492897737,
+                "champion": 51,
+                "queue": 450,
+                "season": 13,
+                "timestamp": 1594054096300,
+                "role": "DUO_SUPPORT",
+                "lane": "TOP"
+            },
+
+            .
+            .
+            .
+
+            {
+                "platformId": "KR",
+                "gameId": 4458940337,
+                "champion": 22,
+                "queue": 420,
+                "season": 13,
+                "timestamp": 1592628733977,
+                "role": "DUO_SUPPORT",
+                "lane": "MID"
+            }
+        ],
+        "startIndex": 0,
+        "endIndex": 100,
+        "totalGames": 183
+    }
+    */
 }
