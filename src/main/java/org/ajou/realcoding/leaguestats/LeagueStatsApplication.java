94630ada8b53ffe177c245e1dad786b0a671e6a2
Hyung-Taik Choi <htc.refactor@gmail.com>
2020-07-14 20:45:54 +0900

    Initialized project via Spring Initializr with Lombok and Spring Web dependencies.

---
+++ src/main/java/org/ajou/realcoding/leaguestats/LeagueStatsApplication.java
@@ -0,0 +1,13 @@
+package org.ajou.realcoding.leaguestats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ajou/realcoding/leaguestats/LeagueStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ajou.realcoding.leaguestats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
