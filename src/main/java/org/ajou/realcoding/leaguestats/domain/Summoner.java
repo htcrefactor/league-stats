c9ca2f10e5126869e3f8937cb8ee5465a5b4858e
Hyung-Taik Choi <htc.refactor@gmail.com>
2020-07-14 21:48:16 +0900

    Added example response body to Summoner class.

--- src/main/java/org/ajou/realcoding/leaguestats/domain/Summoner.java
+++ src/main/java/org/ajou/realcoding/leaguestats/domain/Summoner.java
@@ -1,4 +1,15 @@
 package org.ajou.realcoding.leaguestats.domain;
 
 public class Summoner {
+    /*
+    {
+        "id": "cTJRw-gsfTz7Rd5Yy-MFegnTIyeM2rCIPeWYPgB4hPrrIsk",
+        "accountId": "9hKVAEFNqw2MQ9-9QpRQxPLbqOKfMKU1BvnsC0AZXIfPyIU",
+        "puuid": "l6PK10LSkL_wlwZMyVpi1di81hOYOb93A2FBoHJTmjHkHOT4lCRLAVvU7Eh7gZh0grz71nITQmrdvA",
+        "name": "BLACKGREYWHITE",
+        "profileIconId": 3478,
+        "revisionDate": 1594031959000,
+        "summonerLevel": 262
+    }
+     */
 }
