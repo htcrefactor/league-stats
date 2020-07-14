b8dc1b697ba9d65b27196a2fb9b3f77d75a90753
Hyung-Taik Choi <htc.refactor@gmail.com>
2020-07-14 21:52:12 +0900

    Added example response body to League class.

--- src/main/java/org/ajou/realcoding/leaguestats/domain/League.java
+++ src/main/java/org/ajou/realcoding/leaguestats/domain/League.java
@@ -1,4 +1,38 @@
 package org.ajou.realcoding.leaguestats.domain;
 
 public class League {
+    /*
+    [
+        {
+            "leagueId": "2d916f43-ef5a-4625-8533-f943f7f99c81",
+            "queueType": "RANKED_SOLO_5x5",
+            "tier": "SILVER",
+            "rank": "III",
+            "summonerId": "cTJRw-gsfTz7Rd5Yy-MFegnTIyeM2rCIPeWYPgB4hPrrIsk",
+            "summonerName": "BLACKGREYWHITE",
+            "leaguePoints": 8,
+            "wins": 33,
+            "losses": 38,
+            "veteran": false,
+            "inactive": false,
+            "freshBlood": false,
+            "hotStreak": false
+        },
+        {
+            "leagueId": "e3666efb-49c4-4782-9592-98b6fb1a36e7",
+            "queueType": "RANKED_FLEX_SR",
+            "tier": "SILVER",
+            "rank": "III",
+            "summonerId": "cTJRw-gsfTz7Rd5Yy-MFegnTIyeM2rCIPeWYPgB4hPrrIsk",
+            "summonerName": "BLACKGREYWHITE",
+            "leaguePoints": 32,
+            "wins": 26,
+            "losses": 30,
+            "veteran": false,
+            "inactive": false,
+            "freshBlood": false,
+            "hotStreak": false
+        }
+    ]
+    */
 }
